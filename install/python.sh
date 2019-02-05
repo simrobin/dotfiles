76fb8fef8b7ead762ae9af944870a423d0f18334
Simon Robin <simon@simrobin.fr>
2019-02-05 10:18:32 +0100

    add python installation

---
+++ install/python.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+main() {
+  local PYTHON3_VERSION="3.6.8"
+
+  if [[ ! -d "${HOME}/opt/pyenv" ]]; then
+    git clone --depth 1 https://github.com/pyenv/pyenv.git "${HOME}/opt/pyenv"
+  else
+    pushd "${HOME}/opt/pyenv" && git pull && popd
+  fi
+
+  source "${SCRIPT_DIR}/../sources/_python"
+
+  if command -v pyenv 1>/dev/null 2>&1; then
+    pyenv install "${PYTHON3_VERSION}"
+    pyenv global "${PYTHON3_VERSION}"
+
+    if command -v pip > /dev/null 2>&1; then
+      pip install --user --upgrade pip
+    fi
+  fi
+}
+
+main
