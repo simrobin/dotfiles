5d78d6b848afb99ba32e94f7fab3eb24b41bdb46
Simon Robin <simon.robin.info@gmail.com>
2020-01-27 10:35:01 +0100

    update python_pgcli install script

--- install/python_pgcli.sh
+++ install/python_pgcli.sh
@@ -2,12 +2,35 @@
 
 set -o nounset -o pipefail -o errexit
 
+clean() {
+  sudo rm -rf "${HOME}/.pgpass"
+  sudo rm -rf "${HOME}/.config/pgcli"
+}
+
+credentials() {
+  if ! command -v pass > /dev/null 2>&1; then
+    exit
+  fi
+
+  local PASS_DIR="${PASSWORD_STORE_DIR-${HOME}/.password-store}"
+  local PG_PASS
+  PG_PASS="$(find "${PASS_DIR}" -name "*pgpass.gpg" -print | sed -e "s|${PASS_DIR}/\(.*\)\.gpg$|\1|")"
+
+  if [[ $(echo "${PG_PASS}" | wc -l) -eq 1 ]]; then
+    pass show "${PG_PASS}" > "${HOME}/.pgpass"
+    chmod 600 "${HOME}/.pgpass"
+  fi
+}
+
 install() {
-  local SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  local SCRIPT_DIR
+  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
   source "${SCRIPT_DIR}/../sources/_python"
 
+  local PGCLI_VERSION="2.1.1"
+
   if ! command -v pip > /dev/null 2>&1; then
-    echo "pip is required"
+    printf "pip is required\n"
     exit
   fi
 
@@ -15,11 +38,7 @@ install() {
     sudo apt-get install -y -qq libpq-dev
   fi
 
-  if [[ "$(pip install --help | grep prefer-binary | wc -l)" -eq 1 ]]; then
-    pip install --user --prefer-binary pgcli
-  else
-    pip install --user pgcli
-  fi
+  pip install --user "pgcli==${PGCLI_VERSION}" --only-binary psycopg2
 
   if ! command -v pgcli > /dev/null 2>&1; then
     return
