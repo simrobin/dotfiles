9489a8526cac91bb57bb55fbb0b5e68f4e7704fb
Simon Robin <simon.robin.info@gmail.com>
2020-03-19 18:40:13 +0100

    chore(fzf): update completion and setup

--- install/fzf.sh
+++ install/fzf.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-set -o nounset -o pipefail -o errexit
+# set -o nounset -o pipefail -o errexit
+
+clean() {
+  rm -rf "${HOME}/.fzf.bash"
+  rm -rf "${HOME}/opt/fzf"
+}
 
 install() {
   if ! command -v git > /dev/null 2>&1; then
-    echo "git not found"
+    printf "git not found\n"
     exit
   fi
 
-  if [[ ! -d "${HOME}/opt/fzf" ]]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/opt/fzf"
+  local FZF_INSTALL_DIR="${HOME}/opt/fzf"
+
+  if [[ ! -d ${FZF_INSTALL_DIR} ]]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_INSTALL_DIR}"
   else
-    pushd "${HOME}/opt/fzf" && git pull && popd
+    (cd "${FZF_INSTALL_DIR}" && git pull)
   fi
 
-  "${HOME}/opt/fzf/install" --key-bindings --completion --no-zsh --no-fish --no-update-rc
+  "${FZF_INSTALL_DIR}/install" --key-bindings --completion --no-zsh --no-fish --no-update-rc
 }
